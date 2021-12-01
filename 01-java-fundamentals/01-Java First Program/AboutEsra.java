de9f7ebc590144c29f5cf930eae0aaf351855b0a
Esra Yazar <esrayzr@gmail.com>
2021-11-30 19:03:59 -0500

    First Java Program assignment

---
+++ 01-java-fundamentals/01-Java First Program/AboutEsra.java
@@ -0,0 +1,7 @@
+public class AboutEsra {
+    public static void main(String[] args) {
+        System.out.println("My name is Esra");
+	System.out.println("I am 34 years old");
+	System.out.println("My hometown is Newark,DE");
+    }
+}
