de90577af335f87244da796f4bbffdb100477bc9
Esra Yazar <esrayzr@gmail.com>
2021-12-03 20:17:02 -0500

    basics assignment done

---
+++ 01-java-fundamentals/05-basics/Basics.java
@@ -0,0 +1,95 @@
+import java.util.ArrayList;
+
+public class Basics {
+    public void printNums(){
+        for (int i =1; i<256; i++)
+        System.out.println(i);
+    }
+    public void printOddNumbers(){
+        for (int i=1; i<256; i=i+2)
+        System.out.println(i);
+    }
+
+    public int sigma255(){
+        int sum =0;
+        for(int i=1; i<256; i++){
+            sum=sum+i;
+        }
+        return sum;
+    }
+    public void printIntArray(int arr[]){
+        for (int ele : arr) {
+            System.out.println(ele);
+        }
+    }
+    public void findMax(int arr[]){
+        int max= Integer.MIN_VALUE;
+        for (int ele : arr) {
+            if(ele>max) max=ele;
+        }
+        System.out.println("maxNum=" + max);
+    }
+    public void oddNumbersArray(){
+        ArrayList y = new ArrayList<>();
+       for(int i=1; i<256; i=i+2) y.add(i);
+       System.out.println(y);
+       
+    }
+    public void getAverage(int arr[]){
+        int sum =0;
+        for (int ele : arr) {
+            sum=sum+ele;
+        }
+        if(arr.length!=0){
+            System.out.println("average= " + sum/arr.length);
+        }
+    }
+
+    public int greaterThan(int arr[], int y){
+        int count = 0;
+        for (int ele : arr) {
+            if (ele>y)
+            count++;
+        }
+        return count;
+
+    }
+    public void squareValues(int arr[]){
+        ArrayList y = new ArrayList<>();
+        for(int i=0; i<arr.length; i++){
+            y.add(arr[i]*arr[i]);
+        }
+        System.out.println(y);
+    }
+
+    public void eliminateNegatives(int arr[]){
+        ArrayList y =new ArrayList<>();
+        for (int ele : arr) {
+            if(ele<0)y.add(0);
+            else y.add(ele);
+        }
+        System.out.println(y);
+    }
+
+    public ArrayList maxMinAverage(int arr[]){
+        ArrayList y = new ArrayList<>();
+        int max= Integer.MIN_VALUE;
+        int min= Integer.MAX_VALUE;
+        int sum = 0;
+        for (int ele : arr) {
+            if(ele>max) max=ele;
+            if (ele<min) min=ele;
+            sum = sum+ele;
+        }
+        y.add(max);
+        y.add(min);
+
+        if (arr.length>0){y.add(sum/arr.length);}
+        else y.add(0);
+
+        return y;
+
+    }
+
+
+}
---
+++ 01-java-fundamentals/05-basics/TestBasics.java
@@ -0,0 +1,38 @@
+public class TestBasics {
+    public static  void main(String[] args) {
+        Basics tester = new Basics();
+        tester.printNums();
+        tester.printOddNumbers();
+        System.out.println(tester.sigma255());
+        //iterating through an array
+        int[] testintarray = {1,3,5,7,9,13};
+        tester.printIntArray(testintarray);
+
+        // Max value
+        int[] testintarray2 = {1,3,-19,5,7,9,-5,13,-587};
+        tester.findMax(testintarray2);
+
+        //Array with odd numbers
+        tester.oddNumbersArray();
+
+        //Get Average
+        tester.getAverage(testintarray);
+
+        //Greater than y
+        System.out.println("greater than y: "+tester.greaterThan(testintarray,4));
+
+        //Square the values
+        tester.squareValues(testintarray);
+
+        //Eliminate Negative Numbers
+        tester.eliminateNegatives(testintarray2);
+
+        //Max, Min, Average
+        System.out.println(tester.maxMinAverage(testintarray));
+
+
+
+    }
+    
+    
+}
