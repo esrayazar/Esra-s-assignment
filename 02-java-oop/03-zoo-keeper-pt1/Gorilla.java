31c383f8dbc5f95afbecd7a6f2d661c51509c7da
Esra Yazar <esrayzr@gmail.com>
2021-12-09 20:57:01 -0500

    Mammal and Gorilla created

---
+++ 02-java-oop/03-zoo-keeper-pt1/Gorilla.java
@@ -0,0 +1,19 @@
+public class Gorilla extends Mammal {
+    // public Gorilla(int energyLevel){
+    //     super(energyLevel);
+    // }
+    public void throwSomething(){
+        System.out.println("The gorilla has thrown something");
+        this.energyLevel -= 5;
+    }
+    public void eatBananas(){
+        System.out.println("The gorilla has eatan bananas");
+        this.energyLevel +=10;
+    }
+    public void climb(){
+        System.out.println("The gorilla has been climbed the tree.");
+        this.energyLevel -=10;
+    }
+
+    
+}
---
+++ 02-java-oop/03-zoo-keeper-pt1/TestGorilla.java
@@ -0,0 +1,15 @@
+public class TestGorilla {
+    public static void main(String[] args) {
+        Gorilla sweet = new Gorilla();
+        sweet.throwSomething();
+        sweet.throwSomething();
+        sweet.throwSomething();
+        sweet.eatBananas();
+        sweet.eatBananas();
+        sweet.climb();
+        displayEnergy();
+
+        
+    }
+    
+}
