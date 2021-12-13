b62deaed8b5e1e2f61a3b27b6df0db99d9cc78a6
Esra Yazar <esrayzr@gmail.com>
2021-12-13 14:48:21 -0500

    Object master part1 is finished

---
+++ 02-java-oop/05-object-master-pt1/HumanTest.java
@@ -0,0 +1,11 @@
+public class HumanTest {
+    public static void main(String[] args) {
+        Human human1 = new Human();
+        Human human2 = new Human();
+        System.out.println(human1.getHealth());
+        human1.attack(human2);
+        System.out.println(human1.getHealth());
+
+    }
+    
+}
