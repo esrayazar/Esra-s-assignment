378d455ff40b97d9780784f719ca30c324b9f2de
Esra Yazar <esrayzr@gmail.com>
2022-01-09 13:28:18 -0500

    created createlanguage

--- 03-java-spring/02-spring-data-i/05-group-languages/groupLanguages/src/main/java/com/assignments/groupLanguages/controller/HomeController.java
+++ 03-java-spring/02-spring-data-i/05-group-languages/groupLanguages/src/main/java/com/assignments/groupLanguages/controller/HomeController.java
@@ -2,21 +2,32 @@ package com.assignments.groupLanguages.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
 	
-	@Autowired
-	private LanguageService languageService;
+//	@Autowired
+//	private LanguageService languageService;
 	
 	//All language List
 	@GetMapping("/languages")
 	public String getLanguages(Model model) {
-		List<Language> languages =languageService.getAllLanguages();
-		model.addAttribute("languages", languages)
+//		List<Language> languages =languageService.getAllLanguages();
+//		model.addAttribute("languages", languages)
 		return "index.jsp";
 		
 		}
+	 //create language
+    @PostMapping("/languages")
+    public String createLanguages() {
+        System.out.println("post request is recieved = " + language);
+//        languageService.save(language);
+        //name=Esra&creator=Esra+Yazar&version=1
+        return "redirect:/languages";
+        
+    }
 	
 	
 
