43b057c3899168da1a0b7346e8a0155164d5fc21
Esra Yazar <esrayzr@gmail.com>
2022-01-09 13:36:06 -0500

    service layer created

---
+++ 03-java-spring/02-spring-data-i/05-group-languages/groupLanguages/src/main/java/com/assignments/groupLanguages/service/LanguageService.java
@@ -0,0 +1,13 @@
+package com.assignments.groupLanguages.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LanguageService {
+	
+
+
+}
