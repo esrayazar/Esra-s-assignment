3d48c96d082716c858b13e150fcacb1104285488
Esra Yazar <esrayzr@gmail.com>
2022-01-10 11:27:27 -0500

    Added

--- 03-java-spring/02-spring-data-i/05-group-languages/groupLanguages/src/main/java/com/assignments/groupLanguages/controller/HomeController.java
+++ 03-java-spring/02-spring-data-i/05-group-languages/groupLanguages/src/main/java/com/assignments/groupLanguages/controller/HomeController.java
@@ -1,42 +1,46 @@
 package com.assignments.groupLanguages.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.assignments.groupLanguages.models.Language;
+import com.assignments.groupLanguages.service.LanguageService;
+
 @Controller
 public class HomeController {
 	
-//	@Autowired
-//	private LanguageService languageService;
+	@Autowired
+	private LanguageService languageService;
 	
 	//All language List
 	@GetMapping("/languages")
 	public String getLanguages(Model model) {
-//		List<Language> languages =languageService.getAllLanguages();
-//		model.addAttribute("languages", languages)
+		List<Language> languages = languageService.getAllLanguages();
+		model.addAttribute("languages",languages);
+		
 		return "index.jsp";
 		
 		}
 	 //create language
     @PostMapping("/languages")
     public String createLanguages() {
-        System.out.println("post request is recieved = " + language);
-//        languageService.save(language);
-        //name=Esra&creator=Esra+Yazar&version=1
         return "redirect:/languages";
         
     }
   //shows language details
     @GetMapping("/languages/{id}")
     public String getLanguagesid(@PathVariable("id") Long id,Model model) {
-        System.out.println("languageId = "+ id);
-//        Language language = languageService.getLanguageById(id);
-//        model.addAttribute("language", language);
+       
         return "details.jsp";
     }
+    
+    
 	
 	
 
