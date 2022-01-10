e05fed3fae2c94c56faf9de369bbb2904205a6be
Esra Yazar <esrayzr@gmail.com>
2022-01-09 23:35:44 -0500

    getters and setters generated

--- 03-java-spring/02-spring-data-i/05-group-languages/groupLanguages/src/main/java/com/assignments/groupLanguages/models/Language.java
+++ 03-java-spring/02-spring-data-i/05-group-languages/groupLanguages/src/main/java/com/assignments/groupLanguages/models/Language.java
@@ -1,7 +1,50 @@
 package com.assignments.groupLanguages.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
+@Entity
+@Table(name="languages")
 public class Language{
+	@Id
+	@GeneratedValue(strategy =GenerationType.IDENTITY)
+	
+	private Long id;
+	@Size(min= 2, max=20, message="Name and creator should be between 2-20")
+	private String name;
+	private String creator;
+	@NotEmpty
+	private String version;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCreator() {
+		return creator;
+	}
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+	public String getVersion() {
+		return version;
+	}
+	public void setVersion(String version) {
+		this.version = version;
+	}
+	
 	
 }
 
