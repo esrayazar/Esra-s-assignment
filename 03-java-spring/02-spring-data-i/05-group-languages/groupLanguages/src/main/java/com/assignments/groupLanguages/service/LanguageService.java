a1ff8b9038ae5885b47e470a088189e290677538
Esra Yazar <esrayzr@gmail.com>
2022-01-10 11:12:51 -0500

    save and getall created

--- 03-java-spring/02-spring-data-i/05-group-languages/groupLanguages/src/main/java/com/assignments/groupLanguages/service/LanguageService.java
+++ 03-java-spring/02-spring-data-i/05-group-languages/groupLanguages/src/main/java/com/assignments/groupLanguages/service/LanguageService.java
@@ -5,9 +5,20 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.assignments.groupLanguages.models.Language;
+import com.assignments.groupLanguages.repositories.LanguageRepository;
+
 @Service
 public class LanguageService {
 	
-
+	@Autowired
+	private LanguageRepository languageRepository;
+	
+	public void save(Language language) {
+		languageRepository.save(language);
+	}
+	public List<Language> getAllLanguages(){
+		return languageRepository.findAll();
+	}
 
 }
