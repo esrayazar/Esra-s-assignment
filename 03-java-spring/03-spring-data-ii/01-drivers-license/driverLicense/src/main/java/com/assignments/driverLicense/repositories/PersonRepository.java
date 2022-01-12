da4da3c1bde5ee2e1351ccbb0fe9beed9457423f
Esra Yazar <esrayzr@gmail.com>
2022-01-12 15:39:30 -0500

    personrepository  fixed

--- 03-java-spring/03-spring-data-ii/01-drivers-license/driverLicense/src/main/java/com/assignments/driverLicense/repositories/PersonRepository.java
+++ 03-java-spring/03-spring-data-ii/01-drivers-license/driverLicense/src/main/java/com/assignments/driverLicense/repositories/PersonRepository.java
@@ -5,11 +5,11 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.assignments.driverLicense.models.person;
+import com.assignments.driverLicense.controllers.PersonController;
 
 @Repository                                             
-public interface PersonRepository extends CrudRepository<person, Long>{
-	List<person> findAll();//SELECT *FROM albums
+public interface PersonRepository extends CrudRepository<PersonController, Long>{
+	List<PersonController> findAll();//SELECT *FROM albums
 
 
 	}
