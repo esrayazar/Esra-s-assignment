32345a40064122deaf615edcc9bfcbf2bba274e1
Esra Yazar <esrayzr@gmail.com>
2022-01-12 15:19:13 -0500

    license model created

---
+++ 03-java-spring/03-spring-data-ii/01-drivers-license/driverLicense/src/main/java/com/assignments/driverLicense/models/license.java
@@ -0,0 +1,51 @@
+package com.assignments.driverLicense.models;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="license")
+public class license {
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private Long id;
+	private String person;
+	private String state;
+	private Date expirationDate;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getPerson() {
+		return person;
+	}
+	public void setPerson(String person) {
+		this.person = person;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public Date getExpirationDate() {
+		return expirationDate;
+	}
+	public void setExpirationDate(Date expirationDate) {
+		this.expirationDate = expirationDate;
+	}
+	public license() {
+	}
+	
+	
+	
+
+
+}
