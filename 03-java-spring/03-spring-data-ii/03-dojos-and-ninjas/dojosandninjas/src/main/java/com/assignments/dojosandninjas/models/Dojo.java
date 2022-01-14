2357dc3d42bd0ec7fd5c03b2a9ffd9f373419a72
Esra Yazar <esrayzr@gmail.com>
2022-01-14 14:20:14 -0500

    dojo model created

---
+++ 03-java-spring/03-spring-data-ii/03-dojos-and-ninjas/dojosandninjas/src/main/java/com/assignments/dojosandninjas/models/Dojo.java
@@ -0,0 +1,76 @@
+package com.assignments.dojosandninjas.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name="dojos")
+public class Dojo {
+	@Id
+	@GeneratedValue(strategy =GenerationType.IDENTITY)
+	private Long id;
+	
+	@Size(min=2, max=20, message="Name should be between 2-20 characters")
+	private String name;
+	
+	@Column(updatable=false)
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date createdAt;
+	
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date updatedAt;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+	@PrePersist
+    protected void onCreate(){ //when object is created save the Date that the object is created at
+        this.createdAt = new Date();
+    }
+    @PreUpdate
+    protected void onUpdate(){ //when object is updated save the Date that the object is updated at
+        this.updatedAt = new Date();
+    }
+
+
+}
