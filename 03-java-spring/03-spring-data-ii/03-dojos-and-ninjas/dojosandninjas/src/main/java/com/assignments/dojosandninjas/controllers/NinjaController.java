f9732fdf8ec4d32dc4e9d2f32bb2e58441da9399
Esra Yazar <esrayzr@gmail.com>
2022-01-16 14:40:46 -0500

    bugs fixed

--- 03-java-spring/03-spring-data-ii/03-dojos-and-ninjas/dojosandninjas/src/main/java/com/assignments/dojosandninjas/controllers/NinjaController.java
+++ 03-java-spring/03-spring-data-ii/03-dojos-and-ninjas/dojosandninjas/src/main/java/com/assignments/dojosandninjas/controllers/NinjaController.java
@@ -6,18 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.assignments.dojosandninjas.models.Dojo;
 import com.assignments.dojosandninjas.models.Ninja;
 import com.assignments.dojosandninjas.services.DojoService;
+import com.assignments.dojosandninjas.services.NinjaService;
 
 @Controller
 public class NinjaController {
 	
 	@Autowired
 	private DojoService dojoService;
+	
+	@Autowired
+	private NinjaService ninjaService;
 	
 	@GetMapping("/ninjas/new")
 	public String addNinjaDisp(Model model) {
@@ -28,9 +31,12 @@ public class NinjaController {
 	
 	@PostMapping("ninjas/new")
 	public String addNinjaToDB(Ninja ninja) {
+		ninjaService.save(ninja);
 		System.out.println(ninja.toString());
 		return "redirect:/";
 	}
+	
+	
 	
 	
 
