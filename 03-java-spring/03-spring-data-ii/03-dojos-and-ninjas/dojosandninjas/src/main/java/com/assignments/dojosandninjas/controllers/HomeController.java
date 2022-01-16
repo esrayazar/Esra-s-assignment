f2de778a82803ed4e67fac9e8e4c1669e8c780a7
Esra Yazar <esrayzr@gmail.com>
2022-01-16 14:39:04 -0500

    validation added

--- 03-java-spring/03-spring-data-ii/03-dojos-and-ninjas/dojosandninjas/src/main/java/com/assignments/dojosandninjas/controllers/HomeController.java
+++ 03-java-spring/03-spring-data-ii/03-dojos-and-ninjas/dojosandninjas/src/main/java/com/assignments/dojosandninjas/controllers/HomeController.java
@@ -1,8 +1,14 @@
 package com.assignments.dojosandninjas.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.assignments.dojosandninjas.models.Dojo;
@@ -26,9 +32,21 @@ public class HomeController {
 	}
 	
 	@PostMapping("/dojos/new")
-	public String createDojo(Dojo dojo) {
-		dojoService.save(dojo);
-		return "redirect:/";
+	public String createDojo(@Valid @ModelAttribute("dojo") Dojo dojo , BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			System.out.println("hata= "+result.getFieldError().getDefaultMessage());
+			model.addAttribute("error", result.getFieldError().getDefaultMessage());
+			return "newdojo.jsp";
+		}else {
+			dojoService.save(dojo);
+			return "redirect:/";
+		}
+		
+	}
+	
+	@GetMapping("/dojos/${id}")
+	public String showNinjas(@PathVariable("id") Long id) {
+		return "dojodetail.jsp";
 	}
 	
 	
