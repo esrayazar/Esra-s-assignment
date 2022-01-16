5a99cd275498dfea229adbd66f45a6ee24249df7
Esra Yazar <esrayzr@gmail.com>
2022-01-16 16:17:52 -0500

    getters and setters  added

--- 03-java-spring/03-spring-data-ii/03-dojos-and-ninjas/dojosandninjas/src/main/java/com/assignments/dojosandninjas/models/Ninja.java
+++ 03-java-spring/03-spring-data-ii/03-dojos-and-ninjas/dojosandninjas/src/main/java/com/assignments/dojosandninjas/models/Ninja.java
@@ -4,9 +4,12 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
 
@@ -30,6 +33,15 @@ public class Ninja {
 	@DateTimeFormat(pattern="yyyy-MM-dd")
 	private Date createdAt;
 	
+	
+	public Dojo getDojo() {
+		return dojo;
+	}
+
+	public void setDojo(Dojo dojo) {
+		this.dojo = dojo;
+	}
+
 	@DateTimeFormat(pattern="yyyy-MM-dd")
 	private Date updatedAt;
 	
