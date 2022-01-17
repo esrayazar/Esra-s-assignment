e1a9457251a77b5554b7db0ebde02947f504a3bf
Esra Yazar <esrayzr@gmail.com>
2022-01-17 15:06:18 -0500

    studentservice created

---
+++ projectsexample/projects/src/main/java/com/esra/projects/services/StudentService.java
@@ -0,0 +1,28 @@
+package com.esra.projects.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.esra.projects.models.Student;
+import com.esra.projects.repositories.StudentRepository;
+
+@Service
+public class StudentService {
+
+	@Autowired
+	private StudentRepository studentRepo;
+	
+	//Get All Students
+	private List<Student> getAllStudents(){
+		return studentRepo.findAll();
+	}
+	
+	//Create New Student
+	
+	private Student createStudent(Student newStudent) {
+		return studentRepo.save(newStudent);
+	}
+	
+}
