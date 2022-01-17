6026702d3a69f6ca6293998c240a6bc396efb2e2
Esra Yazar <esrayzr@gmail.com>
2022-01-17 15:08:11 -0500

    studentservice created

---
+++ projectsexample/projects/src/main/java/com/esra/projects/services/ProjectService.java
@@ -0,0 +1,28 @@
+package com.esra.projects.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.esra.projects.models.Project;
+import com.esra.projects.repositories.ProjectRepository;
+
+@Service
+public class ProjectService {
+	
+	@Autowired
+	private ProjectRepository projectRepo;
+
+	//Get All Project
+	private List<Project> getAllProjects(){
+		return projectRepo.findAll();
+	}
+	//Create
+	
+	//Update
+	//Delete
+	//Project Details
+	
+	
+}
