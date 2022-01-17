9d4b15fd2936389a3c67d9f0dac5118b6f0d72fa
Esra Yazar <esrayzr@gmail.com>
2022-01-17 16:26:10 -0500

    controller createstudent created

--- projectsexample/projects/src/main/java/com/esra/projects/controllers/HomeController.java
+++ projectsexample/projects/src/main/java/com/esra/projects/controllers/HomeController.java
@@ -3,8 +3,11 @@ package com.esra.projects.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
+import com.esra.projects.models.Student;
 import com.esra.projects.services.ProjectService;
 import com.esra.projects.services.StudentService;
 
@@ -20,6 +23,20 @@ public class HomeController {
 	public String dashBoard(Model model) {
 		model.addAttribute("projects", pService.getAllProjects());
 		return "/projects/dashboard.jsp";
+	}
+	
+	@GetMapping("/students/new")
+	public String newStudent(@ModelAttribute("student") Student student) {
+		return "/students/new.jsp";
+	}
+	
+	@PostMapping("/students/create")
+	public String createStudent(@Valid @ModelAttribute("student") Student student, BindingResult result) {
+		if (result.hasErrors()) {
+			return "/students/new.jsp";
+		}else {
+			return "redirect:/";
+		}
 	}
 
 }
