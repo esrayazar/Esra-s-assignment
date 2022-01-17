61bd62d9b8d801664d26a0e0e0a432cf090923c7
Esra Yazar <esrayzr@gmail.com>
2022-01-17 16:21:33 -0500

    getall project and newstudent created in controller

--- projectsexample/projects/src/main/java/com/esra/projects/controllers/HomeController.java
+++ projectsexample/projects/src/main/java/com/esra/projects/controllers/HomeController.java
@@ -2,6 +2,8 @@ package com.esra.projects.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.esra.projects.services.ProjectService;
 import com.esra.projects.services.StudentService;
@@ -12,5 +14,12 @@ public class HomeController {
 	private StudentService sService;
 	@Autowired
 	private ProjectService pService;
+	
+	
+	@GetMapping("/")
+	public String dashBoard(Model model) {
+		model.addAttribute("projects", pService.getAllProjects());
+		return "/projects/dashboard.jsp";
+	}
 
 }
