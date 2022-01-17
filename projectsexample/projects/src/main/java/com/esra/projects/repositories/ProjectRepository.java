46d86750168da00bd4f69fd999b1c5acb33a06e2
Esra Yazar <esrayzr@gmail.com>
2022-01-17 15:52:55 -0500

    bugs fixed

--- projectsexample/projects/src/main/java/com/esra/projects/repositories/ProjectRepository.java
+++ projectsexample/projects/src/main/java/com/esra/projects/repositories/ProjectRepository.java
@@ -3,10 +3,11 @@ package com.esra.projects.repositories;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.esra.projects.models.Project;
 
-
+@Repository
 public interface ProjectRepository extends CrudRepository<Project, Long> {
 	
 	//Get all project
