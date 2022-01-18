e970a3c0fdf61e7f3b892e4bb68ac86963b67a89
Esra Yazar <esrayzr@gmail.com>
2022-01-18 11:08:00 -0500

    user model getters and setters created

--- projectsexample/auth/auth/src/main/java/com/esra/auth/models/User.java
+++ projectsexample/auth/auth/src/main/java/com/esra/auth/models/User.java
@@ -28,6 +28,48 @@ public class User {
 	@NotBlank
 	@Size(min=2, max = 200, message="password should be between 2-200 long")
 	private String password;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
 	
 	
 	
