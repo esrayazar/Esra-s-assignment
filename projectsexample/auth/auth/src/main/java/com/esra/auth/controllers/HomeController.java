d05556df9b587ef568a4a718b29e6f2fc7b25515
Esra Yazar <esrayzr@gmail.com>
2022-01-18 12:56:50 -0500

    register a user created

--- projectsexample/auth/auth/src/main/java/com/esra/auth/controllers/HomeController.java
+++ projectsexample/auth/auth/src/main/java/com/esra/auth/controllers/HomeController.java
@@ -1,9 +1,14 @@
 package com.esra.auth.controllers;
 
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.esra.auth.models.User;
 import com.esra.auth.services.UserService;
@@ -11,11 +16,24 @@ import com.esra.auth.services.UserService;
 @Controller
 public class HomeController {
 	@Autowired
-	private UserService userservice;
+	private UserService userService;
 	
 	@GetMapping("/")
 	public String index(@ModelAttribute("user") User user) {
 		return "index.jsp";
+	}
+	
+	//register a user
+	@PostMapping("/registration")
+	public String registerUser(@Valid @ModelAttribute("user") User user, BindingResult results, HttpSession session) {
+		if(results.hasErrors()) {
+			return "index.jsp";
+		}else {
+			User newUser= userService.registerUser(user);
+			session.setAttribute("userId", newUser.getId());
+			
+			return "redirect:/projects";
+		}
 	}
 	
 
