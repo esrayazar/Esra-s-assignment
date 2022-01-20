7535f6081e63e3cc3a5846ac05372826ade9b1c6
Esra Yazar <esrayzr@gmail.com>
2022-01-19 21:37:39 -0500

    edit dojo func added

--- 03-java-spring/03-spring-data-ii/03-dojos-and-ninjas/dojosandninjasnew/dojosandninjas/src/main/java/com/assignments/dojosandninjas/controller/HomeController.java
+++ 03-java-spring/03-spring-data-ii/03-dojos-and-ninjas/dojosandninjasnew/dojosandninjas/src/main/java/com/assignments/dojosandninjas/controller/HomeController.java
@@ -10,45 +10,72 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import com.assignments.dojosandninjas.models.Dojo;
 import com.assignments.dojosandninjas.services.DojoService;
 
 @Controller
 public class HomeController {
-@Autowired
-private DojoService dojoService;
+	@Autowired
+	private DojoService dojoService;
 
-@GetMapping("/")
-public String index(Model model) {
-	model.addAttribute("newDojo", this.dojoService.getAllDojos());
-	return "index.jsp";
-}
-@GetMapping("/dojos/new")
-public String createDojoDisplay(@ModelAttribute("dojo") Dojo dojo) {
-	return "newdojo.jsp";
-}
-
-@PostMapping("/dojos/new")
-public String create(@Valid @ModelAttribute("dojo") Dojo dojo, BindingResult result
-		) {
-	if(result.hasErrors()) {
-		return "newdojo.jsp";
-	} 
-	else { 
-		this.dojoService.create(dojo);
-		return "redirect:/";
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("newDojo", this.dojoService.getAllDojos());
+		return "index.jsp";
 	}
-}
 
-@GetMapping("/edit/{id}")
-public String edit(@PathVariable("id") Long id,@ModelAttribute("dojo") Dojo dojo, Model model){
-	model.addAttribute("dojo",this.dojoService.getOneDojo(id));
-	return "edit.jsp";
-	
-}
+	@GetMapping("/dojos/new")
+	public String createDojoDisplay(@ModelAttribute("dojo") Dojo dojo) {
+		return "newdojo.jsp";
+	}
 
+	@PostMapping("/dojos/new")
+	public String create(@Valid @ModelAttribute("dojo") Dojo dojo, BindingResult result) {
+		if (result.hasErrors()) {
+			return "newdojo.jsp";
+		} else {
+			this.dojoService.create(dojo);
+			return "redirect:/";
+		}
+	}
+//
+//	@GetMapping("/edit/{id}")
+//	public String edit(@PathVariable("id") Long id, @ModelAttribute("dojo") Dojo dojo, Model model) {
+//		model.addAttribute("dojo", this.dojoService.getOneDojo(id));
+//		return "edit.jsp";
+//	}
+//
+//@PostMapping("/edit/{id}")
+//public String edit(@Valid @ModelAttribute ("dojo") Dojo dojo, BindingResult result ,@PathVariable("id")Long id, Model model) {
+//	if(result.hasErrors()) {
+////		model.addAttribute("dojo",this.dojoService.getOneDojo(id));
+//		return "edit.jsp";
+//		
+//	} 
+//	else { 
+//		this.dojoService.editDojo(dojo);
+//		return "redirect:/";
+//	}
+//}
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable("id") Long id, Model model) {
+		model.addAttribute("dojo", this.dojoService.getOneDojo(id));
+		return "edit.jsp";
+	}
 
+	@PutMapping("/edit/{id}")
+	public String editedit(@Valid @ModelAttribute("dojo") Dojo dojo, BindingResult result, @PathVariable("id") Long id,
+			Model model) {
+		if (result.hasErrors()) {
+//	model.addAttribute("dojo",this.dojoService.getOneDojo(id));
+			return "edit.jsp";
 
+		} else {
+			this.dojoService.editDojo(dojo);
+			return "redirect:/";
+		}
+	}
 
 }
