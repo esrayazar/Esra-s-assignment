c69b4fcdc061aff5d7639ac1261411a891edee07
Esra Yazar <esrayzr@gmail.com>
2022-01-19 21:56:42 -0500

    ninja service created

--- 03-java-spring/03-spring-data-ii/03-dojos-and-ninjas/dojosandninjasnew/dojosandninjas/src/main/java/com/assignments/dojosandninjas/services/NinjaService.java
+++ 03-java-spring/03-spring-data-ii/03-dojos-and-ninjas/dojosandninjasnew/dojosandninjas/src/main/java/com/assignments/dojosandninjas/services/NinjaService.java
@@ -1,5 +1,34 @@
 package com.assignments.dojosandninjas.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.assignments.dojosandninjas.models.Ninja;
+import com.assignments.dojosandninjas.repositories.NinjaRepository;
+
+@Service
 public class NinjaService {
+	@Autowired
+	private NinjaRepository ninjaRepo;
+	
+	public List <Ninja> getAllNinjas(){
+		return this.ninjaRepo.findAll();
+	}
+	
+	public Ninja create(Ninja newNinja) {
+		return this.ninjaRepo.save(newNinja);
+	}
+	
+	public Ninja editNinja(Ninja ninja) {
+		return this.ninjaRepo.save(ninja);
+	}
+	public Ninja getOneNinja(Long id) {
+		return this.ninjaRepo.findById(id).orElse(null);
+	}
+	public void deleteNinja(Long id) {
+		this.ninjaRepo.deleteById(id);
+	}
 
 }
