002622fa69046505243586030988474f2d66ba35
Esra Yazar <esrayzr@gmail.com>
2022-01-19 21:36:33 -0500

    edit jsp created

--- 03-java-spring/03-spring-data-ii/03-dojos-and-ninjas/dojosandninjasnew/dojosandninjas/src/main/java/com/assignments/dojosandninjas/controller/HomeController.java
+++ 03-java-spring/03-spring-data-ii/03-dojos-and-ninjas/dojosandninjasnew/dojosandninjas/src/main/java/com/assignments/dojosandninjas/controller/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.assignments.dojosandninjas.models.Dojo;
@@ -39,5 +40,15 @@ public String create(@Valid @ModelAttribute("dojo") Dojo dojo, BindingResult res
 		return "redirect:/";
 	}
 }
+
+@GetMapping("/edit/{id}")
+public String edit(@PathVariable("id") Long id,@ModelAttribute("dojo") Dojo dojo, Model model){
+	model.addAttribute("dojo",this.dojoService.getOneDojo(id));
+	return "edit.jsp";
+	
+}
+
+
+
 
 }
