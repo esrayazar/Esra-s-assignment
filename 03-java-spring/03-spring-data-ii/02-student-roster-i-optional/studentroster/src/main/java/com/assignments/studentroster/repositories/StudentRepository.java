ad8e6423145687feb868a25503a6e5922a19f2c8
Esra Yazar <esrayzr@gmail.com>
2022-01-20 23:27:16 -0500

    bugs fixed

---
+++ 03-java-spring/03-spring-data-ii/02-student-roster-i-optional/studentroster/src/main/java/com/assignments/studentroster/repositories/StudentRepository.java
@@ -0,0 +1,15 @@
+package com.assignments.studentroster.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.assignments.studentroster.models.Student;
+
+@Repository
+public interface StudentRepository extends CrudRepository<Student, Long> {
+
+//	Get list of Users 
+	List<Student> findAll();
+}
