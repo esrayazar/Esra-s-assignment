54154f54d3ab8fab9598b7f10e6c47f9db3950ce
Esra Yazar <esrayzr@gmail.com>
2022-01-21 11:38:32 -0500

    product repository fixed

--- 03-java-spring/03-spring-data-ii/05-products-and-categories/productsandcategories/src/main/java/com/assignments/productsandcategories/repositories/ProductRepository.java
+++ 03-java-spring/03-spring-data-ii/05-products-and-categories/productsandcategories/src/main/java/com/assignments/productsandcategories/repositories/ProductRepository.java
@@ -3,13 +3,17 @@ package com.assignments.productsandcategories.repositories;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.assignments.productsandcategories.models.Product;
 
 
-
+@Repository
 public interface ProductRepository extends CrudRepository <Product, Long> {
 
 //	Get list of Users 
 	List<Product> findAll();
+	
+//	List<Product> findByCategoriesNotContains(Category category);
+	
 }
