55e4164128fad7c018aa0ae16738bc9683145a8f
Esra Yazar <esrayzr@gmail.com>
2022-01-23 14:31:30 -0500

    question repo added

---
+++ 03-java-spring/03-spring-data-ii/07-dojo-overflow/dojooverflow/src/main/java/com/assignments/dojooverflow/repositories/AnswerRepository.java
@@ -0,0 +1,15 @@
+package com.assignments.dojooverflow.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.assignments.dojooverflow.models.Answer;
+
+public interface AnswerRepository  extends CrudRepository <Answer, Long> {
+	 
+		List <Answer> findAll();
+
+	
+
+}
