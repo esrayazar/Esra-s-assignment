068cfaaec24defda5c3014ba201563f3792ff1e7
Esra Yazar <esrayzr@gmail.com>
2022-01-23 14:21:55 -0500

    question model added

---
+++ 03-java-spring/03-spring-data-ii/07-dojo-overflow/dojooverflow/src/main/java/com/assignments/dojooverflow/models/Question.java
@@ -0,0 +1,101 @@
+package com.assignments.dojooverflow.models;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name="questions")
+public class Question {
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private Long id;
+	@NotBlank
+	@Size(min=5, max=500, message="You must be typed question! ")
+	private String question;
+	@Column(updatable =false)
+	@DateTimeFormat(pattern ="yyy-MM-DD HH:mm:ss")
+	private Date createdAt;
+	@DateTimeFormat(pattern ="yyy-MM-DD HH:mm:ss")
+	private Date updatedAt;
+	
+	@OneToMany(mappedBy="question",fetch=FetchType.LAZY)
+	private List<Answer> answers;
+	
+	@ManyToMany(fetch=FetchType.LAZY)
+	@JoinTable(
+			name="tags_questions",
+			joinColumns=@JoinColumn(name="question_id"),
+			inverseJoinColumns=@JoinColumn(name="tag_id"))
+private List <Tag> tags;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+
+	public List<Tag> getTags() {
+		return tags;
+	}
+
+	public void setTags(List<Tag> tags) {
+		this.tags = tags;
+	}
+
+	public List<Answer> getAnswers() {
+		return answers;
+	}
+
+	public void setAnswers(List<Answer> answers) {
+		this.answers = answers;
+	}
+
+	public Question() {
+		super();
+	}
+
+	
+}
