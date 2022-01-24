3cc65314f94fb06599653981ae77367b2f068cfd
Esra Yazar <esrayzr@gmail.com>
2022-01-23 22:17:58 -0500

    question save created

--- 03-java-spring/03-spring-data-ii/07-dojo-overflow/dojooverflow/src/main/java/com/assignments/dojooverflow/services/AnswerService.java
+++ 03-java-spring/03-spring-data-ii/07-dojo-overflow/dojooverflow/src/main/java/com/assignments/dojooverflow/services/AnswerService.java
@@ -8,11 +8,14 @@ import org.springframework.stereotype.Service;
 import com.assignments.dojooverflow.models.Answer;
 import com.assignments.dojooverflow.models.Question;
 import com.assignments.dojooverflow.repositories.AnswerRepository;
+import com.assignments.dojooverflow.repositories.QuestionRepository;
 
 @Service
 public class AnswerService {
 	@Autowired
 	private AnswerRepository answerRepo;
+	@Autowired
+	private QuestionRepository questionRepo;
 	
 	//Create
 		public void createQuestion(Answer answer) {
@@ -25,6 +28,12 @@ public class AnswerService {
 		//Get One Question
 		public Answer getOneQuestion(Long id) {
 			return this.answerRepo.findById(id).orElse(null);
+		}
+		
+		public void saveAnswer(Answer answer) {
+			Question question = questionRepo.findById(answer.getQuestion().getId()).get();
+			question.getAnswers().add(answer);
+			this.answerRepo.save(answer);
 		}
 
 }
